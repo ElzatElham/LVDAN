bc3867f471a69dcd283dc2bcf18280ed9bbf7192
ElzatElham <164830945+ElzatElham@users.noreply.github.com>
2024-09-27 18:08:11 +0800

    Update Train_yolov8.py

--- Train_yolov8.py
+++ Train_yolov8.py
@@ -7,13 +7,28 @@ import numpy as np
 
 
 
+from ultralytics import YOLO
+
+from pathlib import Path
+import numpy as np
+
+
+
+
+
 ## Train the model
-detection_model_path = '/Volumes/ZelinDisk/big/YJT_DA/model/Revolution_best.pt'
+detection_model_path = '/home/Jet/ultralytics-main/2nd_paper/model/iCT_best.pt'  ## pre-trained model
 detection_model = YOLO(detection_model_path)  # build a new model from YAML
+
+
+
+
 results      = detection_model.train(
-    data     = './data/iCT2SOMATOM.yaml',
-    datat    = './data/Unlabeled_SOMATOM.yaml',
-    da       = 0.00006,
+    data     = '/home/Jet/ultralytics-main/2nd_paper/data_yaml/iCT2SOMATOM.yaml',           # source domain data
+    datat    = '/home/Jet/ultralytics-main/2nd_paper/data_yaml/Unlabeled_SOMATOM.yaml',     # target domain data
+    LLDA     = 0.001,                                                                      # parameter for LDA loss
+    LGDA     = 2,                                                                      # parameter for GDA loss
+    LTSD     = 0.001,                                                                      # parameter for TSD loss
     iou      = 0.5,
     conf     = 0.1, 
     epochs   = 200, 
@@ -22,6 +37,7 @@ results      = detection_model.train(
     batch    = 16, 
     fliplr   = 0,
     flipud   = 0,
-    name     = '04.25.1.Revolution2iCT_0.00006', 
-    device   = 'cpu')
+    name     = 'new_model.08.13.iCT2SOMATOM.0.0001',                                       
+    device   = '0,1,2,3')
+
 
